2519c12e4ee00cf4518f445ae8ef5a2292320597
Witalij Berdinskich <radio_rogal@keemail.me>
2022-04-19 21:59:31 +0300

    Add an example

---
+++ example/src/main/java/uk/bot_by/bot/slf4j_demo/BotHandler.java
@@ -0,0 +1,28 @@
+package uk.bot_by.bot.slf4j_demo;
+
+import static org.slf4j.impl.LambdaLogger.AWS_REQUEST_ID;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+public class BotHandler implements RequestHandler<Map<String, Object>, String> {
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+  @Override
+  public String handleRequest(Map<String, Object> input, Context context) {
+    MDC.put(AWS_REQUEST_ID, context.getAwsRequestId());
+    context.getLogger().log("handle request " + context.getAwsRequestId());
+    logger.trace("trace message");
+    logger.debug("debug message");
+    logger.info("info message");
+    logger.warn("warning message");
+    logger.error("error message");
+    return "done";
+  }
+
+}
