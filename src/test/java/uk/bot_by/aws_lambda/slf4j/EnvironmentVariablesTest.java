578f6951049220ed44ed470c30cf4c6a21f7a2f3
Witalij Berdinskich <radio_rogal@keemail.me>
2022-05-01 07:53:48 +0300

    New test: using of environment variables

---
+++ src/test/java/uk/bot_by/aws_lambda/slf4j/EnvironmentVariablesTest.java
@@ -0,0 +1,65 @@
+package uk.bot_by.aws_lambda.slf4j;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.MatchesPattern.matchesPattern;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.MDC;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
+
+@ExtendWith({SystemStubsExtension.class})
+@Tag("slow")
+class EnvironmentVariablesTest {
+
+  @SystemStub
+  private EnvironmentVariables environment;
+
+  @AfterEach
+  void tearDown() {
+    MDC.clear();
+  }
+
+  @DisplayName("Read logger properties from the environment, get logger then print out trace message")
+  @Test
+  void useEnvironmentVariables() {
+    // given
+    // override all properties
+    environment.set("LOG_SHOW_DATE_TIME", "false");
+    environment.set("LOG_DEFAULT_LEVEL", "Trace");
+    environment.set("LOG_LEVEL_IN_BRACKETS", "false");
+    environment.set("LOG_SHOW_NAME", "false");
+    environment.set("LOG_SHOW_SHORT_NAME", "false");
+    environment.set("LOG_SHOW_THREAD_ID", "fasle");
+    environment.set("LOG_SHOW_THREAD_NAME", "false");
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream(100);
+    PrintStream printStream = new PrintStream(outputStream);
+    LambdaLoggerFactory loggerFactory = spy(LambdaLoggerFactory.class);
+
+    doReturn(printStream).when(loggerFactory).getPrintStream();
+
+    MDC.put("AWS_REQUEST_ID", "variables-request-id");
+
+    // when
+    loggerFactory.getLogger("lambda.logger.test").trace("trace message");
+
+    // then
+    printStream.flush();
+    printStream.close();
+    outputStream.toString(StandardCharsets.UTF_8);
+    assertThat(outputStream.toString(StandardCharsets.UTF_8), matchesPattern(
+        "variables-request-id TRACE trace message[\\n\\r]+"));
+  }
+
+}
--- src/test/java/uk/bot_by/aws_lambda/slf4j/PropertiesTest.java
+++ src/test/java/uk/bot_by/aws_lambda/slf4j/PropertiesTest.java
@@ -15,10 +15,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.MDC;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({MockitoExtension.class, SystemStubsExtension.class})
 @Tag("slow")
-class LambdaLoggerFactoryTest {
+class PropertiesTest {
+
+  @SystemStub
+  private EnvironmentVariables environment;
 
   @Spy
   private LambdaLoggerFactory loggerFactory;
@@ -28,16 +34,16 @@ class LambdaLoggerFactoryTest {
     MDC.clear();
   }
 
-  @DisplayName("Get logger then print out debug message")
+  @DisplayName("Read logger properties from the file, get logger then print out debug message")
   @Test
-  void getLogger() {
+  void useLoggerProperties() {
     // given
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream(100);
     PrintStream printStream = new PrintStream(outputStream);
 
     doReturn(printStream).when(loggerFactory).getPrintStream();
 
-    MDC.put("AWS_REQUEST_ID", "request-id");
+    MDC.put("AWS_REQUEST_ID", "properties-request-id");
 
     // when
     loggerFactory.getLogger("lambda.logger.test").debug("debug message");
@@ -47,7 +53,7 @@ class LambdaLoggerFactoryTest {
     printStream.close();
     outputStream.toString(StandardCharsets.UTF_8);
     assertThat(outputStream.toString(StandardCharsets.UTF_8), matchesPattern(
-        "request-id \\d{2}:\\d{2}:\\d{2}\\.\\d{3} \\[main\\] thread=1 \\[DEBUG\\] test - debug message[\\n\\r]+"));
+        "properties-request-id \\d{2}:\\d{2}:\\d{2}\\.\\d{3} \\[main\\] thread=1 \\[DEBUG\\] test - debug message[\\n\\r]+"));
   }
 
 }
