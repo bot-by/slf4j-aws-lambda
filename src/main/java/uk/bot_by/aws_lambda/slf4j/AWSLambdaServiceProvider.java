0baad5c5cdeda96ddde9c7e39ce59135afa85ed8
Witalij Berdinskich <radio_rogal@keemail.me>
2022-08-17 11:01:55 +0300

    Add a module

--- src/main/java/uk/bot_by/aws_lambda/slf4j/AWSLambdaServiceProvider.java
+++ src/main/java/uk/bot_by/aws_lambda/slf4j/AWSLambdaServiceProvider.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 Witalij Berdinskich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.bot_by.aws_lambda.slf4j;
 
 import org.slf4j.ILoggerFactory;
@@ -7,6 +22,13 @@ import org.slf4j.helpers.BasicMarkerFactory;
 import org.slf4j.spi.MDCAdapter;
 import org.slf4j.spi.SLF4JServiceProvider;
 
+/**
+ * Responsible for binding the {@link LambdaLoggerFactory}, the {@link BasicMarkerFactory} and the
+ * {@link BasicMDCAdapter}. This is used by the SLF4J API.
+ *
+ * @author Witalij Berdinskich
+ * @since 2.2.0
+ */
 public class AWSLambdaServiceProvider implements SLF4JServiceProvider {
 
   /**
